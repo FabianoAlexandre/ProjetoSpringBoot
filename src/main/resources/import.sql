ed6ea369a6d535d033bc68cbb643c3f2e3004a77
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-09-01 11:48:32 -0300

    Projeto gerado com tutorial do canal DevSuperior.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informatica');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@hotmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
